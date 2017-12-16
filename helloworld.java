b3aa8dcf5f74c8bf97da5912abb3ed498f4c243c
Derek Ng <derekng330@gmail.com>
2017-12-16 15:55:11 -0800

    testing hello world

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class helloworld{
+
+    public static void main(String args[])
+    {
+    System.out.println("Hello World");
+    System.out.print("Hello World");
+    }
+    
+}
